cbd798ffad71bdf6944f9d4c24afffef54e595e7
PedroGradissimo <gradispe@gmail.com>
2021-08-10 17:23:32 +0100

    Added InOrederIteration class

--- iterators.py
+++ iterators.py
@@ -67,3 +67,34 @@ class PreOrderIterator:
 
     def __iter__(self):
         return self
+
+
+class InOrderIterator:
+
+    def __init__(self, sequence):
+        if not _is_perfect_length(sequence):
+            raise ValueError(
+                f"Sequence of length {len(sequence)} does not represent "
+                "a perfect binary tree with length 2^n - 1"
+            )
+        self._sequence = sequence
+        self._stack = []
+        self._index = 0
+
+    def __next__(self):
+        if (len(self._stack) == 0) and (self._index >= len(self._sequence)):
+            raise StopIteration
+
+        # Push left children onto the stack while possible
+        while self._index < len(self._sequence):
+            self._stack.append(self._index)
+            self._index = _left_child(self._index)
+
+        # Pop from stack and process, before moving to the right child
+        index = self._stack.pop()
+        result = self._sequence[index]
+        self._index = _right_child(index)
+        return result
+
+    def __iter__(self):
+        return self
